16ae44ef469d47751140cb7f36032f5a38acf832
Sebastien Grosjean <public@zencocoon.com>
2011-03-02 20:44:56 +0200

    Ensure proper authorization before updating translations

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -19,6 +19,10 @@ module Tolk
     def authorized_for_locale?(locale = @locale)
       current_translator.locales && (current_translator.locales.include?('all') || current_translator.locales.include?(locale.name.to_s))
     end
+    
+    def ensure_authorized_for_locale
+      redirect_to request.referrer unless authorized_for_locale?
+    end
 
     def ensure_authorized_to_create_locale?
       redirect_to tolk_locales_path unless authorize_to_create_locale?
--- app/controllers/tolk/locales_controller.rb
+++ app/controllers/tolk/locales_controller.rb
@@ -2,13 +2,14 @@ module Tolk
   class LocalesController < Tolk::ApplicationController
     before_filter :find_locale, :only => [:show, :all, :update, :updated]
     before_filter :ensure_no_primary_locale, :only => [:all, :update, :show, :updated]
+    before_filter :ensure_authorized_for_locale, :only => [:update]
     before_filter :ensure_authorized_to_create_locale?, :only => [:create]
 
     def index
       @locales = Tolk::Locale.secondary_locales
       @primary_key_scopes = Tolk::Phrase.all(:order => 'tolk_phrases.key ASC').collect{|phrase| phrase.key.split('.').first}.uniq
     end
-  
+
     def show
       respond_to do |format|
         format.html do
